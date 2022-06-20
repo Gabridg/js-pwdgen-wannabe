9980a6760146cece4f6517a60bd97c872858839a
Gabridg <gabri99.del@gmail.com>
2022-06-20 16:53:17 +0200

    Bonus complete

--- Bonus/Script/script.js
+++ Bonus/Script/script.js
@@ -2,30 +2,37 @@
 
 /*
 
-1- Chiedo all'utente il nome
-2- Chiedo all'utente il cognome
-3- Chiedo all'utente il suo colore preferito
-4- Recupero elemento
-5- Genero la password
+1- Testo iniziale
+2- Chiedo all'utente il nome
+3- Chiedo all'utente il cognome
+4- Chiedo all'utente il suo colore preferito
+5- Recupero elemento
+6- Genero la password
+7- Stampo tutto
 
 */
 
-// 1 - Nome utente
+// 1 - Testo iniziale
+const startText = 'La tua password è: '
+
+// 2 - Nome utente
 const firstName = prompt("Qual'è il tuo nome?", 'Gabriele');
 console.log(firstName);
 
-// 2 - Cognome utente
+// 3 - Cognome utente
 const secondName = prompt("Qual'è il tuo cognome?", 'Superina');
 console.log(secondName);
 
-// 3 - Colore preferito utente
+// 4 - Colore preferito utente
 const favColor = prompt("Qual'è il tuo colore preferito?", 'Verde');
 console.log(favColor);
 
-// 4 -Recupero elemento
-let password = document.getElementById('pwd');
+// 5 -Recupero elemento
+const result = document.getElementById('pwd');
 
-// 5 - Genero la password
+// 6 - Genero la password
 
+const password = firstName + secondName + favColor + '21';
 
-password.innerHTML = "La tua password è: " + firstName + secondName + favColor + '21';
+// 7 - Stampo
+result.innerHTML = startText + `<strong> ${password} </strong>`;
