9b2f9d410bf387ef0d95e6dedc3b69b71ef19d91
Gabridg <gabri99.del@gmail.com>
2022-06-20 16:16:28 +0200

    added javascript

---
+++ Script/script.js
@@ -0,0 +1,18 @@
+//Generare una password per l'utente
+
+/*
+
+1- Chiedo all'utente il nome
+2- Chiedo all'utente il cognome
+3- Chiedo all'utente il suo colore preferito
+4- Genero la password
+
+*/
+
+// 1 - Nome utente
+const firstName = prompt("Qual'è il tuo nome?")
+console.log(firstName)
+
+// 2- Cognome utente
+const secondName = prompt("Qual'è il tuo cognome?")
+console.log(secondName)
