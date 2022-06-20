7c4164cbf4cfa910e033468fa0f35e505dbec249
Gabridg <gabri99.del@gmail.com>
2022-06-20 16:26:55 +0200

    Task complete

--- Script/script.js
+++ Script/script.js
@@ -5,14 +5,27 @@
 1- Chiedo all'utente il nome
 2- Chiedo all'utente il cognome
 3- Chiedo all'utente il suo colore preferito
-4- Genero la password
+4- Recupero elemento
+5- Genero la password
 
 */
 
 // 1 - Nome utente
-const firstName = prompt("Qual'è il tuo nome?")
+const firstName = prompt("Qual'è il tuo nome?", 'Gabriele')
 console.log(firstName)
 
-// 2- Cognome utente
-const secondName = prompt("Qual'è il tuo cognome?")
+// 2 - Cognome utente
+const secondName = prompt("Qual'è il tuo cognome?", 'Superina')
 console.log(secondName)
+
+// 3 - Colore preferito utente
+const favColor = prompt("Qual'è il tuo colore preferito?", 'Verde')
+console.log(favColor)
+
+// 4 -Recupero elemento
+const password = document.getElementById('pwd')
+
+// 5 - Genero la password
+
+
+password.innerText = "La tua password è: " + firstName + secondName + favColor + '21'
